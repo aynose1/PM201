7690225d36d96df08e93399d2d61cf00e78b707d
AlonsoGarcia93 <122043683@upq.edu.mx>
2026-08-19 17:38:32 -0600

    aljañ

--- practica_6/appTest/components/Saludo.js
+++
@@ -1,9 +0,0 @@
-import { Text } from 'react-native';
-
-/* El export es muy importante para permitir importar esta función desde otro archivo */
-export const Saludo = () => {
-    /* Un componente siempre debe regresar algo */
-    return(
-        <Text>Hoal RN: Componente Propio</Text>
-    )
-};
